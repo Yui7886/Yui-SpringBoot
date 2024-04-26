cb41c5848ec26e95da9b2c87745e6fd8775916c8
liuyi <liuyi@zjyinray.com>
2024-04-26 14:56:41 +0800

    initial commit

---
+++ yui-module-system/src/main/java/com/yui/YuiSpringBootApplication.java
@@ -0,0 +1,19 @@
+package com.yui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Yui Spring Boot 应用程序
+ *
+ * @author liuyi
+ * @date 2024/04/26
+ */
+@SpringBootApplication
+public class YuiSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YuiSpringBootApplication.class, args);
+    }
+
+}
---
+++ yui-module-system/src/test/java/com/yui/YuiSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yui;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YuiSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
